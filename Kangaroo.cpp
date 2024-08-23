874b4a011f98bd91ae2dd0312a6668488b021d08
Davi <77292587+See-Ivad@users.noreply.github.com>
2024-08-23 20:20:26 -0300

    Update Kangaroo.cpp

--- Kangaroo.cpp
+++ Kangaroo.cpp
@@ -1,11 +1,3 @@
-//============================================================================
-// Name        : Kangaroo.cpp
-// Author      : dfg
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include "Movable.hpp"
 #include "Obstacle.hpp"
 
@@ -17,23 +9,25 @@ using namespace std;
 
 int main() {
 
-	Vector2f windowSize(200,200);
+	Vector2f windowSize(700,900);
 
-    sf::RenderWindow window(sf::VideoMode(windowSize.y,windowSize.x), "Hello World");
+    sf::RenderWindow window(sf::VideoMode(windowSize.y,windowSize.x), "Kangaroo");
 
     Vector2f position(windowSize.x/2, windowSize.y/2);
     Vector2f velocity(0.0,0.0);
-    Vector2f size(10,10);
+    Vector2f size(40,15);
     Movable player(position, velocity, size);
 
     while (window.isOpen())
     {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)){//Iniciio do "Loop de eventos"
             if (event.type == sf::Event::Closed)
                 window.close();
-        }
+
+        }//Final do "loop de Eventos"
+
+        player.update(&window);
 
         window.clear();
         window.draw(player.getBody());
